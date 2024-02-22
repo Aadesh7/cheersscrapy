fec5288f8c65889ca9fed3d222db23d228af48bb
Aadesh Dhakal <adeutd7@gmail.com>
2024-02-22 23:47:00 +0545

    Initialize repo

---
+++ cheers/settings.py
@@ -0,0 +1,34 @@
+BOT_NAME = "cheers"
+
+SPIDER_MODULES = ["cheers.spiders"]
+NEWSPIDER_MODULE = "cheers.spiders"
+
+# Splash set up
+
+SPLASH_URL = 'http://localhost:8050'
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+}
+
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
+
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+# Items pipeline
+ITEM_PIPELINES = {
+    'cheers.pipelines.CSVPipeline': 100,
+}
+
+REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
+TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+FEED_EXPORT_ENCODING = "utf-8"
